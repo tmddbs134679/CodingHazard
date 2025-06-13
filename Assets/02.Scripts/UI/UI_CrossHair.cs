91548f9d5d24bc1370dbf5f23a7debd3cb6647c2
송승윤 <119779039+tmddbs134679@users.noreply.github.com>
2025-06-13 15:59:50 +0900

    feat : 크로스헤어 작업

--- Assets/02.Scripts/UI/UI_CrossHair.cs
+++ Assets/02.Scripts/UI/UI_CrossHair.cs
@@ -2,6 +2,7 @@ using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI_CrossHair : UI_Base
 {
@@ -11,8 +12,16 @@ public class UI_CrossHair : UI_Base
         AimHair,
         DamageCrossHair,
         KillEffectCrossHair
-
     }
+    enum Images
+    {
+       KillLeft,
+       KillRight,
+       KillUp,
+       KillDown,
+    }
+
+
     private Vector3 _originalScale;
 
     public override bool Init()
@@ -22,6 +31,7 @@ public class UI_CrossHair : UI_Base
 
         #region Object Bind
         BindObject(typeof(GameObjects));
+        BindImage(typeof(Images));
         #endregion
 
 
@@ -34,6 +44,16 @@ public class UI_CrossHair : UI_Base
         _originalScale = GetObject((int)GameObjects.AimHair).GetComponent<RectTransform>().localScale;
         GetObject((int)GameObjects.DamageCrossHair).SetActive(false);
         GetObject((int)GameObjects.KillEffectCrossHair).SetActive(false);
+
+        killCrossHairs.Add(GetImage((int)Images.KillLeft));
+        killCrossHairs.Add(GetImage((int)Images.KillRight));
+        killCrossHairs.Add(GetImage((int)Images.KillUp));
+        killCrossHairs.Add(GetImage((int)Images.KillDown));
+        foreach (var image in killCrossHairs)
+        {
+            _originalKillPositions[image] = image.GetComponent<RectTransform>().anchoredPosition;
+        }
+
     }
 
     public float recoilScaleAmount = 1.3f;
@@ -43,6 +63,7 @@ public class UI_CrossHair : UI_Base
 
         if (Input.GetMouseButtonDown(0))
         {
+            OnAimEnd();
             AnimateRecoilCrosshair(GetObject((int)GameObjects.AimHair));
         }
         else if(Input.GetMouseButtonDown(1))
@@ -54,6 +75,11 @@ public class UI_CrossHair : UI_Base
             GetObject((int)GameObjects.DamageCrossHair).SetActive(true);
             AnimateRecoilCrosshair(GetObject((int)GameObjects.DamageCrossHair));
             AutoHideAfterDelay(GetObject((int)GameObjects.DamageCrossHair));
+        }
+        else if (Input.GetKeyDown(KeyCode.N))
+        {
+            GetObject((int)GameObjects.KillEffectCrossHair).SetActive(true);
+            AnimateKillCrosshair();
         }
     }
 
@@ -85,7 +111,6 @@ public class UI_CrossHair : UI_Base
     {
         crossHair.transform.DOKill(); 
 
-        // 활성화 보장
         if (!crossHair.activeSelf)
             crossHair.SetActive(true);
 
@@ -94,4 +119,49 @@ public class UI_CrossHair : UI_Base
             .SetDelay(delay)
             .OnComplete(() => crossHair.SetActive(false));
     }
+
+
+    private List<Image> killCrossHairs =new List<Image>();
+    private Dictionary<Image, Vector2> _originalKillPositions = new Dictionary<Image, Vector2>();
+    private Dictionary<Image, Tween> _activeTweens = new Dictionary<Image, Tween>();
+    [SerializeField] private float moveDistance = 100f; 
+    [SerializeField] private float fadeDuration = 0.5f;
+    public void AnimateKillCrosshair()
+    {
+        foreach (var image in killCrossHairs)
+        {
+            if (image == null) continue;
+
+            RectTransform rt = image.GetComponent<RectTransform>();
+            Vector2 startPos = _originalKillPositions[image];
+            Vector2 dir = startPos.normalized;
+            Vector2 targetPos = startPos + dir * moveDistance;
+
+        
+            if (_activeTweens.ContainsKey(image))
+            {
+                _activeTweens[image]?.Kill();
+                _activeTweens.Remove(image);
+            }
+
+
+            image.gameObject.SetActive(true);
+            rt.anchoredPosition = startPos;
+            rt.DOKill(); 
+            image.DOFade(1f, 0f); 
+
+            Sequence seq = DOTween.Sequence();
+            seq.Join(rt.DOAnchorPos(targetPos, fadeDuration).SetEase(Ease.OutCubic));
+            seq.Join(image.DOFade(0f, fadeDuration).SetEase(Ease.InQuad));
+            seq.AppendCallback(() => {
+                rt.anchoredPosition = startPos;
+
+                _activeTweens.Remove(image);
+            });
+
+            _activeTweens[image] = seq;
+        }
+    }
+
+
 }
