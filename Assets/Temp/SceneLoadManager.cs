6aa5de1c32b1ec6d76d9366306bd4feff824efa9
lumos <206481784+lumos-gna@users.noreply.github.com>
2025-06-16 00:53:41 +0900

    feat : 로딩씬, 매니저 구현

--- Assets/Temp/SceneLoadManager.cs
+++ Assets/Temp/SceneLoadManager.cs
@@ -5,29 +5,55 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoadManager : Singleton<SceneLoadManager>
 {
-    private float time = 0;
+    public float CurProgressRatio { get; private set; }
+
+    private string loadingScene = "LoadingScene";
+    
+    private float minLoadingTime = 2;
+    
+    private WaitForSeconds startLoadingTime = new (1);
+    
+    
     
     public void LoadScene(string name)
     {
+        CurProgressRatio = 0;
+ 
+        SceneManager.LoadScene(loadingScene);
+
         StartCoroutine(LoadingAsync(name));
     }
 
+    
     IEnumerator LoadingAsync(string name)
     {
+        yield return startLoadingTime;
+
         AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(name);
         
-        asyncOperation.allowSceneActivation = false; 
-        
-        while(!asyncOperation.isDone)
-        { 
-            time += Time.deltaTime; 
-            
-            print(asyncOperation.progress); 
-           
-            if(time > Constants.MinLoadingTime){ 
-                asyncOperation.allowSceneActivation = true; 
+        asyncOperation.allowSceneActivation = false;
+
+        float timer = 0f;
+        float fakeProgress = 0f;
+
+    
+        while (!asyncOperation.isDone)
+        {
+            timer += Time.deltaTime;
+
+            float targetProgress = Mathf.Clamp01(asyncOperation.progress / 0.9f);
+
+            fakeProgress = Mathf.MoveTowards(fakeProgress, targetProgress, Time.deltaTime);
+
+            CurProgressRatio = fakeProgress;
+
+            if (fakeProgress >= 0.9f && timer >= minLoadingTime)
+            {
+                asyncOperation.allowSceneActivation = true;
             }
+
             yield return null;
         }
     }
+  
 }
