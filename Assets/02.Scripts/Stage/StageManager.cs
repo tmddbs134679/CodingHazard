eaf28357fb2f975606b58d1cb8b23fbb954b67ed
lumos <206481784+lumos-gna@users.noreply.github.com>
2025-06-16 19:53:00 +0900

    feat : 스테이지 목표과제 구현, 스테이지 클리어 구조 임시용 구현

--- Assets/02.Scripts/Stage/StageManager.cs
+++ Assets/02.Scripts/Stage/StageManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Events;
 
 public class StageManager : MonoBehaviour
@@ -9,12 +10,22 @@ public class StageManager : MonoBehaviour
     public static StageManager Instance => _instance;
 
     private static StageManager _instance;
-    
-    public event UnityAction OnCollectedItem;
 
+    public event UnityAction<StageObjective> OnObjectiveUpdatedProgress;
+    public event UnityAction<StageObjective> OnChangedObjective;
+    
 
     [field: SerializeField] public PlayerController PlayerController { get; private set; }
 
+    public List<StageObjective> Objectives => objectives;
+
+    [SerializeField] private List<StageObjective> objectives;
+    
+    [SerializeField] private FadeScreen fadeScreen;
+    
+    [SerializeField] private GameObject tempResultUI;
+    [SerializeField] private Button tempResultButton;
+    
 
     private void Awake()
     {
@@ -28,12 +39,55 @@ public class StageManager : MonoBehaviour
         }
     }
 
-    public void CollectItem()
+    public void UpdateObjectives<T>(T target) where T : MonoBehaviour
     {
-        OnCollectedItem?.Invoke();
+        if (objectives.Count > 0)
+        {
+            var objective = objectives[0];
+            
+            if (objective.TryUpdateProgress(target, out bool isComplete))
+            {
+                OnObjectiveUpdatedProgress?.Invoke(objective);
+
+                
+                if (isComplete)
+                {
+                    if (objectives.Count == 1)
+                    {
+                        ClearStage();
+                    }
+                    else
+                    {
+                        var nextObjective = objectives[1];
+                        
+                        OnChangedObjective?.Invoke(nextObjective);
+                    }
+                    
+                    objectives.RemoveAt(0);
+                }
+            }
+        }
     }
 
     public void ClearStage()
     {
+        fadeScreen.FadeOut(
+            () =>
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+                
+                fadeScreen.gameObject.SetActive(false);
+                tempResultUI.gameObject.SetActive(true);
+            });
+        
+        tempResultButton.onClick.AddListener(() =>
+        {
+            SceneLoadManager.Instance.LoadScene("LobbyScene");
+
+            Time.timeScale = 1;
+        });
+        
+        Time.timeScale = 0;
     }
 }
