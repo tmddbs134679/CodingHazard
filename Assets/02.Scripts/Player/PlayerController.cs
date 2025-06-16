6f0e91426c2ff1925cdd25cde6e3409a7f8a4e71
UHANKNAG <hhs095615@gmail.com>
2025-06-16 10:30:13 +0900

    feat: Crouch Animation & Player Controller Refactor

--- Assets/02.Scripts/Player/PlayerController.cs
+++ Assets/02.Scripts/Player/PlayerController.cs
@@ -2,21 +2,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    #region InputSystem
     public PlayerInputs playerInput { get; private set; }
     public PlayerInputs.PlayerActions playerActions { get; private set; }
-    public PlayerStateMachine stateMachine { get; private set; }
     
-    public Animator Animator { get; private set; }
-    public CharacterController Controller { get; private set; }
-    public ForceReceiver ForceReceiver { get; private set; }
+    #endregion
 
-    
-    [SerializeField, Range(-90f, 0f)] private float minXLook = -60f;
-    [SerializeField, Range(0f, 90f)] private float maxXLook = 30f;
-    [SerializeField, Range(50f, 300f)] private float lookSensitivity = 100f;
-    public Transform playerTrans;
-    private Transform camTrans;
-    private float xRotation = 0f;
+    #region State Machine & State Flags
+    public PlayerStateMachine stateMachine { get; private set; }
     
     public bool isWalking = false;
     public bool isWalkingHold => playerActions.Walk.IsPressed();
@@ -31,32 +24,45 @@ public class PlayerController : MonoBehaviour
     public bool isAttacking = false;
     public bool isJumping = false;
     public bool isJumpPressed => playerActions.Jump.WasPressedThisFrame();
-    
-    
+
+    #endregion
+
+    #region Camera Look
+    [SerializeField, Range(-90f, 0f)] private float minXLook = -60f;
+    [SerializeField, Range(0f, 90f)] private float maxXLook = 30f;
+    [SerializeField, Range(50f, 300f)] private float lookSensitivity = 100f;
+    public Transform playerTrans;
+    private Transform camTrans;
+    private float xRotation = 0f;
+    #endregion
+
+    #region Camera Move
     private Vector2 curMovementInput;
     [SerializeField] private float moveSpeed;
     [SerializeField] private FPSVirtualCamera fpsVirtualCamera;
+    #endregion
+
+    #region Components
+    public Animator Animator { get; private set; }
+    public CharacterController Controller { get; private set; }
+    public ForceReceiver ForceReceiver { get; private set; }
+    #endregion
     
     private void Awake()
     {
         playerInput = new PlayerInputs();
         playerActions = playerInput.Player;
+        
         stateMachine = new PlayerStateMachine(this);
         Animator = GetComponentInChildren<Animator>();
         Controller = GetComponent<CharacterController>();
         ForceReceiver = GetComponent<ForceReceiver>();
+        
         stateMachine.MainCamTransform = Camera.main?.transform;
     }
 
-    private void OnEnable()
-    {
-        playerInput.Enable();
-    }
-
-    private void OnDisable()
-    {
-        playerInput.Disable();
-    }
+    private void OnEnable() => playerInput.Enable();
+    private void OnDisable() => playerInput.Disable();
     
     private void Start()
     {
@@ -71,47 +77,23 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        // Player 시선에 따른 카메라 이동
         Look();
-        stateMachine.CurrentState.Update();
         
+        // Crouch 판정 및 처리
         if (playerActions.Sit.WasPressedThisFrame())
         {
             isCrouching = !isCrouching; // 토글
-            if (isCrouching)
-            {
-                Debug.Log("Crouch");
-            }
-            else
-            {
-                Debug.Log("UnCrouch");
-            }
-        }
-        
-        
-        curMovementInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        moveSpeed = stateMachine.curMoveSpeed;
-        
-        if (Input.GetMouseButton(0))
-        {
-            fpsVirtualCamera.PlayRecoilToFire(Vector3.one);
-        }
-        
-        if (Input.GetMouseButtonDown(1))
-        {
-            fpsVirtualCamera.ZoomIn(-20f, 0.5f);
-        }
-        
-        if (Input.GetMouseButtonUp(1))
-        {
-            fpsVirtualCamera.ZoomOut(0.5f);
+            Crouch(isCrouching);
         }
 
-        if (curMovementInput != Vector2.zero)
-        {
-            fpsVirtualCamera.UpdateHeadBob(moveSpeed);
-        }
+        // Camera 특수 효과
+        CameraMove();
+        
+        stateMachine.CurrentState.Update();
     }
     
+    // Animation Clip 길이 구하기
     public float GetAnimationClipLength(string clipName)
     {
         var clips = Animator.runtimeAnimatorController.animationClips;
@@ -138,6 +120,53 @@ public class PlayerController : MonoBehaviour
 
         camTrans.localRotation = Quaternion.Euler(xRotation, 0f, 0f); // 상하
         playerTrans.Rotate(Vector3.up * mouseX); // 좌우
+    }
+
+    private void CameraMove()
+    {
+        curMovementInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveSpeed = stateMachine.curMoveSpeed;
+        
+        if (Input.GetMouseButton(0))
+        {
+            fpsVirtualCamera.PlayRecoilToFire(Vector3.one);
+        }
+        
+        if (Input.GetMouseButtonDown(1))
+        {
+            fpsVirtualCamera.ZoomIn(-20f, 0.5f);
+        }
+        
+        if (Input.GetMouseButtonUp(1))
+        {
+            fpsVirtualCamera.ZoomOut(0.5f);
+        }
+
+        if (curMovementInput != Vector2.zero)
+        {
+            fpsVirtualCamera.UpdateHeadBob(moveSpeed);
+        }
+    }
+
+    void Crouch(bool isCrouch)
+    {
+        if (isCrouch)
+        {
+            Debug.Log("Crouch");
+            // 추후 isCrouching 값에 따라 조절하도록 구조 조정할 것
+            Animator.SetBool("isCrouching", true);
+            Controller.height = 1.1f;
+            Controller.center = new Vector3(0, 0.6f, 0f);
+        }
+        else
+        {
+            Debug.Log("UnCrouch");
+            Animator.SetBool("isCrouching", false);
+            Controller.height = 1.9f;
+            Controller.center = new Vector3(0, 1f, 0f);
+        }
+        
+        // Crouch일 때의 카메라 이동 추가
     }
     
     public void Attack()
