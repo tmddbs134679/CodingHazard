be0267fd4f15018c57a78c5f5eef53f53d6e127a
UHANKNAG <hhs095615@gmail.com>
2025-06-17 10:27:28 +0900

    feat: Recover Condition

--- Assets/02.Scripts/Player/PlayerCondition.cs
+++ Assets/02.Scripts/Player/PlayerCondition.cs
@@ -12,6 +12,8 @@ public class PlayerCondition : MonoBehaviour
     {
         hp.curValue = hp.maxValue = 100f;
         stamina.curValue = stamina.maxValue = 50f;
+
+        StartCoroutine(RecoverCondition());
     }
 
     private void Update()
@@ -23,6 +25,29 @@ public class PlayerCondition : MonoBehaviour
         }
     }
     
+    private IEnumerator RecoverCondition()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1f);
+
+            if (hp.curValue < hp.maxValue)
+            {
+                AddHp(5f);
+            }
+            
+            if (stamina.curValue < stamina.maxValue)
+            {
+                AddStamina(5f); // 초당 5 회복
+            }
+        }
+    }
+
+    public void AddHp(float amount)
+    {
+        hp.curValue = Mathf.Min(hp.curValue + amount, hp.maxValue);
+    }
+    
     public void TakeDamage(float amount)
     {
         hp.curValue = Mathf.Max(hp.curValue - amount, 0f);
@@ -30,7 +55,7 @@ public class PlayerCondition : MonoBehaviour
     
     }
 
-    public void RecoverStamina(float amount)
+    public void AddStamina(float amount)
     {
         stamina.curValue = Mathf.Min(stamina.curValue + amount, stamina.maxValue);
     }
