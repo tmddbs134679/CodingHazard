c57dc28aacfdb1a4fe2bd4f42bc9c29b146f0bae
송승윤 <119779039+tmddbs134679@users.noreply.github.com>
2025-06-17 11:28:30 +0900

    feat : StageManager랑 연결

--- Assets/02.Scripts/UI/ClearUIController.cs
+++ Assets/02.Scripts/UI/ClearUIController.cs
@@ -5,12 +5,22 @@ using UnityEngine;
 public class ClearUIController : MonoBehaviour
 {
     [SerializeField] private UI_ClearPopup _popup;
-    // Start is called before the first frame update
-    void Start()
+
+    private void OnEnable()
     {
-        
+        StageManager.Instance.OnStageClear += Play;
     }
 
+    private void OnDisable()
+    {
+        StageManager.Instance.OnStageClear -= Play;
+    }
+
+
+    private void Play()
+    {
+        StartCoroutine(PlaySequence());
+    }
     private IEnumerator PlaySequence()
     {
         yield return _popup.FadeInCoroutine();             // 1. FadeIn 완료 대기
